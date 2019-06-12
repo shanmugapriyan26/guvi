a4e84731c6cc228571ade0281d753268e683d550
shanmugapriyan26 <51733712+shanmugapriyan26@users.noreply.github.com>
2019-06-12 12:32:18 +0530

    Update vowels.py

--- codekata/vowels.py
+++ codekata/vowels.py
@@ -2,7 +2,7 @@ s=input()
 x=['a','A','i','I','o','O','e','E','u','U']
 y=['b','B','c','C','d','D','F','f','g','G','h','H','j','J','k','K','l','L','m','M','n','N','p','P','q','Q','r','R','s','S','t','T','v','V','w','W','x','X','y','Y','z','Z']
 if s in x:
-  print('Vowels')
+  print('Vowel')
 elif s in y:
   print('Consonant')
 else:
