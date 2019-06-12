2df2ebae5a05c1b8fbe50d76314561bd26e0919d
shanmugapriyan26 <51733712+shanmugapriyan26@users.noreply.github.com>
2019-06-12 12:40:16 +0530

    Create alphabet.py

---
+++ codekata/alphabet.py
@@ -0,0 +1,6 @@
+s=input()
+x=['a','A','i','I','o','O','e','E','u','U''b','B','c','C','d','D','F','f','g','G','h','H','j','J','k','K','l','L','m','M','n','N','p','P','q','Q','r','R','s','S','t','T','v','V','w','W','x','X','y','Y','z','Z']
+if s in x:
+  print('Alphabet')
+else:
+  print('No')
